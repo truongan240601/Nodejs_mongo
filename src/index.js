2a656d2b30981d11ff4813f9fc31d934cc63e43d
an <119092751+truongan240601@users.noreply.github.com>
2024-01-02 15:53:11 +0700

    push

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+const { engine } = require ('express-handlebars');
+const morgan = require('morgan')
+const port = 3000
+
+app.use(express.static(path.join(__dirname,'public')));
+app.use(morgan('combined'))
+
+
+// template engine
+app.engine('hbs', engine({extname:'.hbs'}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname,'resources/views'));
+
+app.get('/', (req, res) => {
+  res.render('home');
+})
+app.get('/news', (req, res) => {
+  res.render('news');
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
